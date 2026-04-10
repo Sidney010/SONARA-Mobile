0d4f26964668752aaf6d3098416377172d3c04c5
SIDNEY CAMPOS ARAGÃO <25132876@CFP127ALUNO.LOCAL>
2026-04-10 13:23:20 -0300

    Enviando organização de pastas

---
+++ app/src/main/java/com/example/sonara/screens/StartBemVindoScreen.kt
@@ -0,0 +1,21 @@
+package com.example.sonara.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun StartBemVindoScreen(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color.Red)
+    ){
+        Column() { }
+    }
+
+}
