73a8e14390a19cf1b3e8d3021decac3550a35a11
Sidney010 <sidneyc.aragao@gmail.com>
2026-04-14 23:49:26 -0300

    Implementação de animação no button

--- app/src/main/java/com/example/sonara/core/ui/components/AppButton.kt
+++ app/src/main/java/com/example/sonara/core/ui/components/AppButton.kt
@@ -1,13 +1,17 @@
 package com.example.sonara.core.ui.components
 
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -16,13 +20,35 @@ fun AppButton(
     text: String,
     onClick: () -> Unit
 ) {
+    val interactionSource = remember { MutableInteractionSource() }
+    val isPressed by interactionSource.collectIsPressedAsState()
+
+    val scale by animateFloatAsState(
+        targetValue = if (isPressed) 0.96f else 1f,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        ),
+        label = ""
+    )
+
+    val elevation by animateDpAsState(
+        targetValue = if (isPressed) 2.dp else 4.dp,
+        label = ""
+    )
+
     Button(
         modifier = modifier
             .fillMaxWidth(0.5f)
-            .height(50.dp),
+            .height(50.dp)
+            .graphicsLayer {
+                scaleX = scale
+                scaleY = scale
+            },
+        onClick = onClick,
+        interactionSource = interactionSource,
         shape = MaterialTheme.shapes.medium,
-        elevation = ButtonDefaults.buttonElevation(4.dp),
-        onClick = onClick
+        elevation = ButtonDefaults.buttonElevation(elevation)
     ) {
         Text(text)
     }
